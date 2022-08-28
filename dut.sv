4b264d61b666e22c82e819bed6578f8dc93a1cc3
adinavale <68146367+adinavale@users.noreply.github.com>
2022-08-27 22:10:20 -0700

    Created first assertion

--- dut.sv
+++ dut.sv
@@ -3,6 +3,8 @@
 //
 module dut(intf.idut ix);
 
+logic [3:0] old_state;
+
 default clocking @(posedge(ix.clk));
 
 endclocking
@@ -10,4 +12,19 @@ endclocking
 always @(posedge(ix.clk)) begin
   $display(ix.state);
 end
+
+always @ (ix.state) begin
+  case(ix.state) 
+    1: begin
+        assert (old_state == 0 || old_state == 3 || old_state == 5) 
+        else begin
+          $display("Error: Legal transition to state 0 can only occur from old_state 0, 3, or 5");
+        end
+      end
+  endcase
+
+  old_state = ix.state;
+end
+
+
 endmodule
